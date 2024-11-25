3c5893fd19e933ab3b3eb07c476eadf42b3ab961
Letee2 <pablofp.33@gmail.com>
2024-11-25 11:00:07 +0100

    fix: Añadido decorador para permitir migraciones iniciales

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 from .models import UserProfile
 from django.contrib.auth.models import User
 
@@ -7,5 +9,8 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'telefono']
     search_fields = ['user__username', 'telefono']
 
-for user in User.objects.all():
-    UserProfile.objects.get_or_create(user=user)
+# Create UserProfiles after migrations are complete
+@receiver(post_migrate)
+def create_user_profiles(sender, **kwargs):
+    for user in User.objects.all():
+        UserProfile.objects.get_or_create(user=user)
